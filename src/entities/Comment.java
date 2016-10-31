b4c2d97c05524e70ebcbdf9359c7de1ea070abe8
Lepec1n <stas33553377@yandex.ru>
2016-10-31 22:20:53 +0300

    Added Comment, Transaction

---
+++ src/entities/Comment.java
@@ -0,0 +1,87 @@
+package entities;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Stanislav on 31.10.2016.
+ * stas33553377@yandex.ru
+ */
+public class Comment implements Entity {
+
+    private int id;
+    private int user_id;
+    private int game_id;
+    private int score;
+    private String content;
+    private Date c_Date;
+
+    public Comment(){}
+    public Comment(int user_id, int game_id, int score, String content, Date c_Date){
+        if (c_Date == null || content == null)
+            throw new NullPointerException("initialization info for Comment is invalid");
+
+        this.user_id = user_id;
+        this.game_id = game_id;
+        this.score = score;
+        this.content = content;
+        this.c_Date = c_Date;
+    }
+    public Comment(int id, int user_id, int game_id,int score,String content, Date c_Date) {
+        this(user_id,game_id,score,content,c_Date);
+        this.id = id;
+    }
+
+    @Override
+    public Entity fromResultSet(ResultSet rs) {
+        try {
+            this.id = rs.getInt("Id");
+            this.user_id = rs.getInt("User_id");
+            this.game_id = rs.getInt("Game_id");
+            this.score = rs.getInt("Score");
+            this.content = rs.getString("Content");
+            this.c_Date = rs.getDate("C_Date");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+    public int getGame_id() {
+        return game_id;
+    }
+    public int getScore() {
+        return score;
+    }
+    public String getContent() {
+        return content;
+    }
+    public Date getc_Date() {
+        return c_Date;
+    }
+
+    public void setUser_id(int u_id) {
+        this.user_id = u_id;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void setGame_id(int game_id) {
+        this.game_id = game_id;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public void setc_Date(Date c_Date) {
+        this.c_Date = c_Date;
+    }
+}
---
+++ src/entities/Transaction.java
@@ -0,0 +1,69 @@
+package entities;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Stanislav on 31.10.2016.
+ * stas33553377@yandex.ru
+ */
+public class Transaction implements Entity{
+
+    private int id;
+    private int user_id;
+    private int sum;
+    private Date t_Date;
+
+    public Transaction(){}
+    public Transaction(int user_id, int sum, Date t_Date){
+        if (t_Date == null)
+            throw new NullPointerException("initialization info for Transaction is invalid");
+
+        this.user_id = user_id;
+        this.sum = sum;
+        this.t_Date = t_Date;
+    }
+    public Transaction(int id, int user_id, int sum, Date t_Date) {
+        this(user_id,sum,t_Date);
+        this.id = id;
+    }
+
+    @Override
+    public Entity fromResultSet(ResultSet rs) {
+        try {
+            this.id = rs.getInt("Id");
+            this.user_id = rs.getInt("User_id");
+            this.sum = rs.getInt("Sum");
+            this.t_Date = rs.getDate("T_DATE");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+    public int getSum() {
+        return sum;
+    }
+    public Date getT_Date() {
+        return t_Date;
+    }
+
+    public void setUser_id(int u_id) {
+        this.user_id = u_id;
+    }
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+    public void setT_Date(Date t_Date) {
+        this.t_Date = t_Date;
+    }
+}
